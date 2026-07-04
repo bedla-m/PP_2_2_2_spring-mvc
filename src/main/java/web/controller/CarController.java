150372cd0780e50761c69d0088321bc93dbcdf90
Kirill <kornienko.krll@gmail.com>
2026-07-04 17:34:16 +0400

    Update class html

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,12 +1,12 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
 import web.service.CarService;
-import web.service.CarServiceImpl;
 
 import java.util.List;
 
@@ -14,8 +14,9 @@ import java.util.List;
 public class CarController {
     private final CarService carService;
 
-    public CarController() {
-        this.carService = new CarServiceImpl();
+    @Autowired
+    public CarController(CarService carService) {
+        this.carService = carService;
     }
 
     @GetMapping("/cars")
